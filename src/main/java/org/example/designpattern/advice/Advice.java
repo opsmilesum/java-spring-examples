57f3858bd6d2e98061a1715ef8c71f01ed4a43de
Peng Ou <2896371938@qq.com>
2022-01-26 14:39:43 +0800

    remove idea generated files

--- src/main/java/org/example/designpattern/advice/Advice.java
+++
@@ -1,9 +0,0 @@
-package org.example.designpattern.advice;
-
-public interface Advice<T> {
-  void onEnter();
-
-  void onSuccess(T t);
-
-  void onError(Exception e);
-}
--- src/main/java/org/example/designpattern/converter/ConverterInterface.java
+++
@@ -1,8 +0,0 @@
-package org.example.designpattern.converter;
-
-import java.time.Instant;
-import java.util.Map;
-
-public interface ConverterInterface {
-  Instant convert(Instant initial, Map<String, Object> context);
-}
